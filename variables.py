0c8ecd11fe542029064b95c10fe197e74e31f047
Isaac Finotti <imisaac2807@gmail.com>
2023-07-29 12:48:33 -0300

    First commit

---
+++ variables.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+ROOTDIR = Path(__file__).parent
+FILESDIR = ROOTDIR / 'files'
+LOGOPATH = FILESDIR / 'logo.png'
+
+
+# colors
+PRIMARY_COLOR = '#1e81b0'
+DARKER_COLOR = '#16658a'
+DARKEST_COLOR = '#115270'
+
+
+BIGGER_FONT = 40
+MEDIUM_FONT = 24
+SMALL_FONT = 16
+
+DEFAULT_MARGIN = 15
+
+MINIMUM_WIDTH = 200
